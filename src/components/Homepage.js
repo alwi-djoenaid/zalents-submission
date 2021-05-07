98243f6bd3f6da477530ddc7baedf6833d7b6142
alwi-djoenaid <alwi.djoenaid@gmail.com>
2021-05-07 11:21:18 +0700

    modal for add new task item, display only not functional

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,9 +1,12 @@
-import { Button, Card, CardContent, ClickAwayListener, Container, Fade, Grid, Grow, Menu, MenuItem, MenuList, Paper, Popper, Typography } from '@material-ui/core';
+import { Backdrop, Button, Card, CardContent, ClickAwayListener, Container, Fade, Grid, Grow, Menu, MenuItem, MenuList, Modal, Paper, Popper, TextField, Typography } from '@material-ui/core';
 import React, { useEffect , useRef, useState} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import OptionIcon from '../assets/images/dots.png';
 import todoContainer from '../container/TodoContainer';
-import DoneIcon from '../assets/images/checklist.png'
+import DoneIcon from '../assets/images/checklist.png';
+import CircleIcon from '../assets/images/circle.png';
+import PlusIcon from '../assets/images/plus.png';
+import AddIcon from '../assets/images/AddIcon.png';
 
 const styles = makeStyles(theme => ({
     root: {
@@ -72,16 +75,23 @@ const styles = makeStyles(theme => ({
         display: 'flex',
         justifyContent: 'space-between'
     },
+    progressDisplay: {
+        display: 'flex'
+    },
     progressBar: {
         width: '84px',
         height: '8px',
         borderRadius: '8px',
         backgroundColor: '#F5F5F5',
         margin: 'auto 0px'
+
     },
     progressPercentage: {
         height: '100%',
         borderRadius: '8px',
+    },
+    percentageNumber: {
+        marginLeft: '9px'
     },
     button: {
         minWidth: '0'
@@ -116,48 +126,68 @@ const styles = makeStyles(theme => ({
         fontSize: '12px',
         lineHeight: '22px',
         fontWeight: 'normal'
+    },
+    addBtn: {
+        marginTop: '15px',
+        display: 'flex',
+    },
+    newTaskText: {
+        color: '#2F3136',
+        fontSize: '14px',
+        lineHeight: '20px',
+        fontWeight: 'normal',
+        top: '50%',
+        paddingLeft: '9px',
+        paddingTop: '2px'
+    },
+    modal: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    paper: {
+        backgroundColor: theme.palette.background.paper,
+        padding: theme.spacing(2, 4, 3),
+        width: '567px',
+        // height: '302.5px',
+        boxShadow: '0px 3px 6px -4px #0000001F',
+        boxShadow: '0px 6px 16px 0px #00000014',
+        boxShadow: '0px 9px 28px 8px #0000000D'
+    },
+    modalTextField: {
+        padding: '10px'
     }
 }))
 
 const Homepage = props => {
     const classes = styles();
     const TodoContainer = todoContainer.useContainer();
-    const anchorRef = useRef(null)
-    //const prevOpen = useRef(open);
-    const [anchorEl, setAnchorEl] = useState(null); 
+    const [anchorEl, setAnchorEl] = useState(null);
+    const anchorRef = useRef(null);
     const open = Boolean(anchorEl);
+    const prevOpen = useRef(open);
     const [placement, setPlacement] = useState();
     const id = open ? 'transitions-popper' : undefined;
-    let parentTask = {
-
-    };
+    const [openModal, setOpenModal] = useState(false);
     let subTask = [];
 
-    // const handleClick = (newPlacement) => (event) => {
-    //     setAnchorEl(event.currentTarget);
-    //     setOpen((prev) => placement !== newPlacement || !prev);
-    //     setPlacement(newPlacement);
-    // };
+    const handleOpen = () => {
+        setOpenModal(true);
+    };
+
+    const handleClose = () => {
+        setOpenModal(false);
+      };
 
     const handleClick = (event) => {
         setAnchorEl(anchorEl ? null : event.currentTarget);
       };
 
     const openMenu = () => {
-        //setOpen((prevOpen) => !prevOpen);
     }
 
-    const handleClose = () => {
-        setAnchorEl(null);
-    };
 
     useEffect(() => {
-        // if (prevOpen.current === true && open === false) {
-        //     anchorRef.current.focus();
-        // }
-
-        // prevOpen.current = open;
-
         const loadData = async () => {
             TodoContainer.getTodoList()
             .then(response => {
@@ -217,19 +247,20 @@ const Homepage = props => {
                                                         <Typography className={classes.taskItemName}>{taskList.name}</Typography>
                                                         <div className={classes.bottom}>
                                                             <div className={classes.utility}>
-                                                                <div className={classes.progressBar}>
-                                                                    <div className={classes.progressPercentage} style={{width: `${taskList.progress_percentage}%`, 
-                                                                        backgroundColor:  `${taskList.progress_percentage == 100 ? '#52C41A' : '#1890FF'}`}}>
+                                                                <div className={classes.progressDisplay}>
+                                                                    <div className={classes.progressBar}>
+                                                                        <div className={classes.progressPercentage} style={{width: `${taskList.progress_percentage}%`, 
+                                                                            backgroundColor:  `${taskList.progress_percentage == 100 ? '#52C41A' : '#1890FF'}`}}>
+                                                                        </div>
                                                                     </div>
                                                                     
-                                                                </div>
-
-                                                                <div>
-                                                                    {taskList.progress_percentage == 100 ?
-                                                                        <img src={DoneIcon} style={{marginRight: '30px', marginTop: '4px'}} />
-                                                                        :
-                                                                        <span className={classes.percentageText}>{taskList.progress_percentage}%</span>
-                                                                    }
+                                                                    <div className={classes.percentageNumber}>
+                                                                        {taskList.progress_percentage == 100 ?
+                                                                            <img src={DoneIcon} style={{marginTop: '4px'}} />
+                                                                            :
+                                                                            <span className={classes.percentageText}>{taskList.progress_percentage}%</span>
+                                                                        }
+                                                                    </div>
                                                                 </div>
 
                                                                 <div className={classes.button}>
@@ -269,7 +300,15 @@ const Homepage = props => {
                                                 }
                                             </div>
                                         )}
-
+                                            <Button type="button" onClick={handleOpen}>
+                                                <div className={classes.addBtn}>
+                                                    <div>
+                                                        <img src={AddIcon}/>
+                                                    </div>
+                                                    <Typography className={classes.newTaskText}>New Task</Typography>
+                                                </div>
+                                            </Button>
+                                            
                                     </CardContent>
                                 </Card>
                             </Grid>
@@ -277,6 +316,43 @@ const Homepage = props => {
                     </Grid>
                 </div>
             </div>
+            {/* Modal for create new task */}
+            <Modal
+                className={classes.modal}
+                aria-labelledby="transition-modal-title"
+                aria-describedby="transition-modal-description"
+                closeAfterTransition
+                open={openModal}
+                onClose={handleClose}
+                BackdropComponent={Backdrop}
+                BackdropProps={{
+                    timeout: 500,
+                }}
+            >
+                <Fade in={openModal}>
+                    <div className={classes.paper}>
+                        <h2 id="transition-modal-title">Create Task</h2>
+                        <div>
+                            <div >
+                                <p style={{marginBottom: '4px'}}>Task Name</p>
+                                <TextField placeholder="example: Build rocket to Mars." variant="outlined" style={{width: '100%'}}></TextField>
+                            </div>
+                            <div >
+                                <p style={{marginBottom: '4px'}}>Progress</p>
+                                <TextField placeholder="0%" variant="outlined" style={{width: '100px'}}></TextField>
+                            </div>
+                            <div style={{paddingTop: '10px', float: 'right'}}>
+                                <Button variant="contained" style={{backgroundColor: '#FFFFFF', borderColor: '1px solid', color: '#262626', marginRight: '8px'}}>
+                                    Cancel
+                                </Button>
+                                <Button variant="contained" style={{backgroundColor: '#27AE60', color: '#FFFFFF'}}>
+                                    Save Task
+                                </Button>
+                            </div>
+                        </div>
+                    </div>
+                </Fade>
+            </Modal>
         </div>
         
     );
